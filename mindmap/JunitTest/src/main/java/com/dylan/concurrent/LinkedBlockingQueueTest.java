537997b0997dc1e8eb7d48412fb92c72812a0cd2
Dylan Liu <dylan_liufq@163.com>
2016-11-21 19:20:15 +0800

    add concurrent notes

---
+++ mindmap/JunitTest/src/main/java/com/dylan/concurrent/LinkedBlockingQueueTest.java
@@ -0,0 +1,10 @@
+package com.dylan.concurrent;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class LinkedBlockingQueueTest {
+
+  public static void main(String...strings){
+    LinkedBlockingQueue<String>l = new LinkedBlockingQueue<String>();
+  }
+}

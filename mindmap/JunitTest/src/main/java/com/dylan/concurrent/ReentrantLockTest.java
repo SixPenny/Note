a804f2d1f47cef59a862e93422a91bb3fe219e9a
Dylan Liu <dylan_liufq@163.com>
2016-11-29 15:06:55 +0800

    dead lock

--- mindmap/JunitTest/src/main/java/com/dylan/concurrent/ReentrantLockTest.java
+++ mindmap/JunitTest/src/main/java/com/dylan/concurrent/ReentrantLockTest.java
@@ -4,7 +4,15 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class ReentrantLockTest {
 
-  public static void main(){
+  public static void main() throws InterruptedException{
     ReentrantLock lock = new ReentrantLock();
+    lock.lockInterruptibly();
+    try{
+      //do something
+    }finally{
+      if(lock.isHeldByCurrentThread()){
+        lock.unlock();
+      }
+    }
   }
 }
